720942f6d72131995fc092c8585c4e0e5261d1cc
LucianoAlbanes <26262333+LucianoAlbanes@users.noreply.github.com>
2022-09-07 16:18:08 -0300

    Improved: Heap as PriorityQueue

--- tp3-busquedas-no-informadas/code/lib/Queues.py
+++ tp3-busquedas-no-informadas/code/lib/Queues.py
@@ -1,46 +1,47 @@
-from collections import deque
+from heapq import heappop, heappush
 
 
 class PriorityQueue:
-    # Placeholder, poor implemented. See heap-tree
-    # Return min value
+    # https://docs.python.org/3/library/heapq.html#priority-queue-implementation-notes
+    __slots__ = ('list', 'count')
+
+    def __init__(self):
+        self.list = []
+        self.count = 0
+
+    def push(self, priority: int, value):
+        self.count += 1
+        return heappush(self.list, (priority, self.count, value))
+
+    def pop(self):
+        v = heappop(self.list)  # Remove counter
+        return v[0], v[2]
+
+    def __len__(self):
+        return self.list.__len__()
+
+
+class Queue:  # FIFO
     __slots__ = 'list'
 
     def __init__(self):
         self.list = []
 
-    def push(self, priority: int, value):
-        self.list.append((priority, value))
+    def push(self, value):
+        self.list.append(value)
 
     def pop(self):
-        self.list.sort(key=lambda t: t[0])
         return self.list.pop(0)
 
     def __len__(self):
         return self.list.__len__()
 
 
-class Queue:    # FIFO
+class Stack:  # LIFO
     __slots__ = 'list'
 
     def __init__(self):
-        self.list = deque()
-
-    def push(self, value):
-        self.list.append(value)
-
-    def pop(self):
-        return self.list.popleft()
-
-    def __len__(self):
-        return self.list.__len__()
-
-
-class Stack:    # LIFO
-    __slots__ = 'list'
-
-    def __init__(self):
-        self.list = deque()
+        self.list = []
 
     def push(self, value):
         self.list.append(value)
