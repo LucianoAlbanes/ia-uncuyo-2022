72859e2c0b7d541dcbc4ffdae9e31f734585e241
LucianoAlbanes <26262333+LucianoAlbanes@users.noreply.github.com>
2022-09-14 01:55:14 -0300

    Added: fast_h() seems working

--- tp5-busquedas-locales/code/NQueens.py
+++ tp5-busquedas-locales/code/NQueens.py
@@ -34,36 +34,9 @@ def calc_rows(board, col):
     return attacked_pairs
 
 
-# def h(board):
-#     # This functions calculates the pairs of queens under attack across the board.
-#
-#     attacked_pairs = 0
-#     n = len(board)
-#     # Columns not needed, problem relaxed, one queen per column.
-#
-#     # Rows
-#     for col in range(n):
-#         for i in range(col + 1, n):
-#             if board[col] == board[i]:
-#                 attacked_pairs += 1
-#
-#     # Main Diagonals
-#     for col in range(n - 1):
-#         for i in range(1, min(n - board[col] - 1, n - col - 1)):
-#             if board[col] + i == board[col + i]:
-#                 attacked_pairs += 1
-#
-#     # Secondary diagonals
-#     for col in range(n):
-#         for i in range(1, min(n - board[col], col + 1)):
-#             if board[col] + i == board[col - i]:
-#                 attacked_pairs += 1
-#
-#     return attacked_pairs
-
-
 def h(board):
     # This functions calculates the pairs of queens under attack ACROSS the board.
+    # Only use if needed, there is a h_fast() function bellow.
     attacked_pairs = 0
 
     # Note: could parallelize
@@ -76,59 +49,69 @@ def h(board):
 
 
 def h_fast(board, col, pos, h_prev):
-    # Need fixing - Don't use
-
     # This function calculates the pairs on queens under attack across the board, after a given movement in ONE queen.
     # The calculation in a lot cheaper than h(), because only checks involved queens after the movement.
     # To perform this calculation, is required the previous number of queens under attack.
+    # Its horrible, but should work. There is a bruteforce test.
 
     # Some variables
     original_pos = board[col]
     n = len(board)
-    involved_before = 0
-    involved_after = 0
 
     # Before
-    # Row
-    for i in range(n):
-        if board[i] == board[col]:
-            involved_before += calc_rows(board, i)
-
-    # Main diag
-    d = max(col - board[col], board[col] - col)
-    for i in range(n-d):
-        if d+i == board[i]:
-            involved_before += calc_main_diag(board, i)
-
-    # Secondary diag
-    d = min(n - col -1, 999)
-    for i in range(d):  # 0-3
-        if d-i == board[d-i]:
-            involved_before += calc_sec_diag(board, 3)
+    involved_before = delta(board, col)
 
     # After
     board[col] = pos
-    # Row
-    for i in range(n):
-        if board[i] == board[col]:
-            involved_after += calc_rows(board, i)
-
-    # Main diag
-    d = max(col - board[col], board[col] - col)
-    for i in range(n-d):
-        if d+i == board[i]:
-            involved_after += calc_main_diag(board, i)
-
-    # Secondary diag
-    d = min(col + board[col], board[col])
-    for i in range(n - d):  # 0-3
-        if board[col] + i == board[col - i]:
-            involved_after += calc_sec_diag(board, i+d)
+    involved_after = delta(board, col)
 
     # Rollback changes
     board[col] = original_pos
 
+    # Return calculated value
     return h_prev - involved_before + involved_after
+
+
+def delta(board, col):
+    n = len(board)
+    counter = 0
+
+    # Row
+    for i in range(n):
+        if board[i] == board[col]:
+            counter += calc_rows(board, i)
+            break
+
+    # Main diagonal
+    dc = col
+    dr = board[col]
+
+    while dc != 0 and dr != 0:
+        dc -= 1
+        dr -= 1
+
+    d = max(dc, dr)
+    for i in range(n - d):
+        if dr+i == board[dc + i]:
+            counter += calc_main_diag(board, dc + i)
+            break
+
+    # Secondary diagonal
+    dc = col
+    dr = board[col]
+
+    while dc != n-1 and dr != 0:
+        dc += 1
+        dr -= 1
+
+    d = max(dc, dr, 1)
+    for i in range(n - dr):
+        if dr+i == board[d-i]:
+            counter += calc_sec_diag(board, d-i)
+            break
+
+    # Return
+    return counter
 
 
 def str_board(board):
@@ -144,9 +127,9 @@ def str_board(board):
     return txt
 
 
-def hill_climbing(board, max_steps, goal_fn=h):
+def hill_climbing(board, max_steps):
     n = len(board)
-    best_value = goal_fn(board)
+    best_value = h(board)
     steps = 0
 
     # Hill climbing
@@ -156,7 +139,7 @@ def hill_climbing(board, max_steps, goal_fn=h):
 
         # Try each column and row
         for col in range(n):
-            tmp = board[col]    # Save the original value
+            tmp = board[col]
             for row in range(n):
                 steps += 1
 
@@ -164,9 +147,8 @@ def hill_climbing(board, max_steps, goal_fn=h):
                 if row == tmp:
                     continue
 
-                # Move and evaluate
-                board[col] = row
-                value = goal_fn(board)
+                # Evaluate
+                value = h_fast(board, col, row, best_value)
 
                 # Verify if better
                 if value < best_value:
@@ -185,23 +167,50 @@ def hill_climbing(board, max_steps, goal_fn=h):
     return best_value, board, steps
 
 
+def bruteforce_fast_h(size):
+    count = 0
+    while True:
+        board = random.sample(range(size), size)
+        a = random.randint(0, size - 1)
+        b = random.randint(0, size - 1)
+
+        # Real queens under attack (expensive)
+        base = h(board)
+
+        # calc h using h_fast. Moves queen at col 'a' to row 'b'
+        fast = h_fast(board, a, b, base)
+
+        # calc same movement, but expensive
+        board[a] = b
+
+        # Break if fails
+        if fast != h(board):
+            break
+        count += 1
+        print(count)
+
+    print(str_board(board))
+    print(f'{h(board)=}, {fast=}')
+
+
 if __name__ == '__main__':
-    k = 8
-    b = random.sample(range(k), k)
-    h_reached, b, steps = hill_climbing(b, math.inf, h)
-    # print(f'{b=}, {h_reached=}')
-    # print(str_board(b))
-
-    solutions = set()
-    for i in range(100*k):
-        random.shuffle(b)
-        h_reached, b, steps = hill_climbing(b, math.inf, h)
-
-        while h_reached > 0:
-            random.shuffle(b)
-            h_reached, b, steps = hill_climbing(b, math.inf, h)
-
-        solutions.add(tuple(b))
-
-    print(solutions)
-    print(len(solutions))
+    # k = 8
+    # b = random.sample(range(k), k)
+    # h_reached, b, steps = hill_climbing(b, math.inf)
+    # # print(f'{b=}, {h_reached=}')
+    # # print(str_board(b))
+    #
+    # solutions = set()
+    # for i in range(10*k):
+    #     random.shuffle(b)
+    #     h_reached, b, steps = hill_climbing(b, math.inf)
+    #
+    #     while h_reached > 0:
+    #         random.shuffle(b)
+    #         h_reached, b, steps = hill_climbing(b, math.inf)
+    #
+    #     solutions.add(tuple(b))
+    #
+    # print(solutions)
+    # print(len(solutions))
+    bruteforce_fast_h(100)
