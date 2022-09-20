e82d708422f540e8e9f6e957cbfcc28c78beff26
LucianoAlbanes <26262333+LucianoAlbanes@users.noreply.github.com>
2022-09-19 21:37:34 -0300

    WIP: Genetic Algorithm

--- tp5-busquedas-locales/code/NQueens.py
+++ tp5-busquedas-locales/code/NQueens.py
@@ -3,24 +3,21 @@ import random
 from BoardUtils import h, h_fast, board_to_str
 
 
-def hill_climbing(board: list, max_steps):
-    """
-    :param board: The board to be analyzed, In-Place
-    :param max_steps: The maximum amount of states to explore. (Verified once per iteration).
-    """
-    n = len(board)
+def hill_climbing(size: int, max_iterations: int):
+    # Variables
+    board = random.sample(range(size), size)
     best_value = h(board)
     h_history = [(0, best_value)]
-    steps = 0
+    iterations = 0
 
     # Hill climbing
-    while best_value != 0 and steps < max_steps:
+    while best_value != 0 and iterations < max_iterations:
         # Best neighbor
         better_neighbors = []
 
         # Try each column and row
-        for col in range(n):
-            for row in range(n):
+        for col in range(size):
+            for row in range(size):
                 # Skip current state
                 if row == board[col]:
                     continue
@@ -32,35 +29,29 @@ def hill_climbing(board: list, max_steps):
                 if value < best_value:
                     better_neighbors.append((value, col, row))
 
-        # Register steps
-        steps += n * n - n
+        # Register iterations
+        iterations += size * size - size
 
         # Check results
         if better_neighbors:
             best_value, col, row = random.choice(better_neighbors)
-            h_history.append((steps, best_value))
+            h_history.append((iterations, best_value))
             board[col] = row
         else:
             break
 
     # Return
-    return best_value, steps, h_history
+    return board, best_value, iterations, h_history
 
 
-def simulated_annealing(board: list, max_iterations, init_temp=700, alpha=0.95):
-    """
-    :param board: The board to be analyzed, In-Place
-    :param max_iterations: The maximum amount of iterations to perform
-    :param init_temp: The starting temperature. Default=700
-    :param alpha: Cooling factor. Must be smaller than 1. Default=0.95
-    """
-
-    n = len(board)
+def simulated_annealing(size: int, max_iterations: int, init_temp=700, alpha=0.95):
+    # Variables
+    board = random.sample(range(size), size)
     best_value = h(board)
     h_history = [(0, best_value)]
     temp = init_temp
     iterations = 0
-    memo = dict()
+    memo = {}
 
     # Simulated Annealing
     for time in range(max_iterations):
@@ -75,8 +66,8 @@ def simulated_annealing(board: list, max_iterations, init_temp=700, alpha=0.95):
         iterations += 1
 
         # Get random successor
-        col = random.randint(0, n-1)
-        row = random.randint(0, n-1)
+        col = random.randint(0, size-1)
+        row = random.randint(0, size-1)
         try:
             value = memo[(col, row)]
         except KeyError:
@@ -96,16 +87,89 @@ def simulated_annealing(board: list, max_iterations, init_temp=700, alpha=0.95):
             memo[(col, row)] = value
 
     # Return
-    return best_value, iterations, h_history
+    return board, best_value, iterations, h_history
+
+
+def genetic_algorithm(size: int, max_iterations: int, population_size=1000, mutation_rate=0.75):
+    # Population must be divisible by 2
+    if population_size % 2 != 0:
+        population_size += 1
+
+    # Variables
+    iterations = population_size
+    fit_objetive = int((size - 1) * size / 2)
+    board = []
+    best_value = 0
+    h_history = []
+
+    # Generate random population and calculate fitness (h)
+    population = [random.sample(range(size), size) for _ in range(population_size)]
+    population_h: list[int] = [0] * population_size
+    for i in range(population_size):
+        fitness = fit_objetive - h(population[i])
+
+        # Check if better solution was found
+        if fitness > best_value:
+            board = population[i]
+            best_value = fitness
+
+        # Append to weights list
+        population_h[i] = fitness
+    h_history.append((0, fit_objetive - best_value))
+
+    # Genetic Algorithm
+    # Run iterations
+    while best_value < fit_objetive and iterations < max_iterations:
+        new_population = []
+        for _ in range(int(population_size/2)):
+            # Select two random parents, weighted by non-attacking queens
+            x, y = None, None
+            while x is y:
+                selected = random.choices(population, population_h, k=2)
+                x = selected[0]
+                y = selected[1]
+
+            # Crossover
+            cut = random.randint(1, size-2)
+            children = [
+                x[0:cut] + y[cut:size],
+                y[0:cut] + x[cut:size]
+            ]
+
+            # Mutation and save to new_population
+            for child in children:
+                if random.uniform(0, 1) >= mutation_rate:
+                    child[random.randint(0, size-1)] = random.randint(0, size-1)
+                new_population.append(child)
+
+        # Update new population
+        population = new_population
+        for i in range(population_size):
+            fitness = fit_objetive - h(population[i])
+
+            # Check if better solution was found
+            if fitness > best_value:
+                board = population[i]
+                best_value = fitness
+
+            # Append to weights list
+            population_h[i] = fitness
+
+        # Increment iterations count and update history
+        iterations += population_size
+        h_history.append((iterations, fit_objetive - best_value))
+
+    # Return best solution found
+    return board, (fit_objetive-best_value), iterations, h_history
 
 
 if __name__ == '__main__':
-    sizes = [4, 8, 10]
-    algorithms = [hill_climbing, simulated_annealing]
+    sizes = [4, 8, 10, 30]
+    algorithms = [hill_climbing, simulated_annealing, genetic_algorithm]
 
     for algorithm in algorithms:
         print(f'~~~Algorithm: {algorithm.__name__}~~~')
         for size in sizes:
-            board = random.sample(range(size), size)
-            best_h, steps, hi = algorithm(board, size**4)
+            # Run and print results
+            board, best_h, steps, hi = algorithm(size, size**4)
             print(f'{size= }, {best_h= }, {steps= }, {board= }\n{board_to_str(board)}\n')
